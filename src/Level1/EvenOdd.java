e16c09bc5aa9cb95d68f98e4b35307514b89563c
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-05-03 12:00:17 +0545

    even and odd

--- src/Level1/EvenOdd.java
+++ src/Level1/EvenOdd.java
@@ -1,5 +1,44 @@
 package Level1;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class EvenOdd {
+    public Map<String, Stack<Integer>> compute(int[] arr) {
+        Stack<Integer> odd = new Stack<>();
+        Stack<Integer> even = new Stack<>();
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                even.push(num);
+            } else {
+                odd.push(num);
+            }
+        }
+     Map<String,Stack<Integer>> map=new HashMap<>();
+       map.put("odd", odd);
+       map.put("even", even);
+
+        return map;
+    }
+
+
+    public static void main(String args[]){
+        EvenOdd obj= new EvenOdd();
+        int[] arr={3,5,44,3,5,4,22,44,3,55};
+       Map<String ,Stack<Integer>> map= obj.compute(arr);
+       for(Map.Entry<String ,Stack<Integer>>temp: map.entrySet()){
+           Stack<Integer> odds=temp.getValue();
+           System.out.print(temp.getKey());
+           while(!odds.isEmpty()){
+               System.out.print(odds.pop());
+           }
+           System.out.println();
+
+
+       }
+
+    }
 
 }
+
--- src/Level1/MaxMin.java
+++ src/Level1/MaxMin.java
@@ -7,28 +7,33 @@ public class MaxMin {
       static  void find(int[] nums){
         int min=nums[0];
         int max=0;
-        //first way
-        for (int i = 0; i < nums.length; i++) {
-                if( min > nums[i]){
-                    min= nums[i];
-                }else if(max < nums[i]){
-                    max=nums[i];
-                }
-             
 
-            
-        }
+        //first way
+//        for (int i = 0; i < nums.length; i++) {
+//                if( min > nums[i]){
+//                    min= nums[i];
+//                }else if(max < nums[i]){
+//                    max=nums[i];
+//                }
+//        }
 
         //second way
         int maximum= Arrays.stream(nums).max().getAsInt();
         int minimum= Arrays.stream(nums).min().getAsInt();
-
-
           System.out.println("max is"+ max + "min is "+ min);
           System.out.println("max2 is" + maximum + "min2 is " + minimum);
 
-
-      }
+          for(int num: nums){
+              if(max < num){
+                  max=num;
+              }
+              if(min > num){
+                  min=num;
+              }
+          }
+          System.out.println("maximum is "+max );
+          System.out.println("minimum is "+min );
+    }
     public static void main(String[] args) {
         MaxMin.find(new int[]{3,5,42,5,2,5,});
 
